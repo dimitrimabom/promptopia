0e08fef9d39f0bf5e92a67ac289a2682b214c2fd
Dimitri Mabom <93844569+dimitrimabom@users.noreply.github.com>
2024-09-08 20:08:19 +0000

    provider done.

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,35 +1,6 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
+import { authOptions } from "@utils/Auth";
 
-const prisma = new PrismaClient();
+const handler = NextAuth(authOptions);
 
-export const authOptions = {
-  // Configuration de Google Provider pour l'authentification
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-  ],
-  // Utilise Prisma comme adaptateur pour NextAuth
-  adapter: PrismaAdapter(prisma),
-  
-  // Option pour stocker les sessions dans un JWT ou une DB
- 
-  
-  // Gestion des callbacks pour ajouter des infos à la session
-  callbacks: {
-    async session({ session, token }) {
-      // Associe l'ID de l'utilisateur au token de session
-      session.user.id = token.sub;
-      return session;
-    },
-  },
-  
-  // Assure-toi d'avoir un secret pour sécuriser l'authentification
-  secret: process.env.NEXTAUTH_SECRET,
-};
-
-export default NextAuth(authOptions);
+export { handler as GET, handler as POST };
